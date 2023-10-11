afb53c0d826955d47f124cc7ba0bba83fa0e1a76
A01661651 <105233241+A01661651@users.noreply.github.com>
2023-10-11 12:54:18 -0600

    Graficas

--- src/components/Graphs/StartQuizGraph.jsx
+++ src/components/Graphs/StartQuizGraph.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
+import { Doughnut } from 'react-chartjs-2';
 import Link from 'next/link'
 import Head from 'next/head'
 import Image from 'next/image'
@@ -14,13 +15,85 @@ import styles from '@/assets/styles/Page.module.css'
 export default function StartQuizGraph() {
     const [] = useState(null);
 
+    const cakeGraph = {
+        labels: ['Totalmente De Acuerdo', 'De Acuerdo', 'Ni Acuerdo ni Desacuerdo', 'Desacuerdo', 'Totalmente en Desacuerdo'],
+        datasets: [
+            {
+                label: 'Resultados Cuestionario Inicial - Pasión y Compromiso',
+                data: [2,4,3,2,1],
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    const cakeGraph2 = {
+        labels: ['Totalmente De Acuerdo', 'De Acuerdo', 'Ni Acuerdo ni Desacuerdo', 'Desacuerdo', 'Totalmente en Desacuerdo'],
+        datasets: [
+            {
+                label: 'Resultados Cuestionario Inicial - Habilidades Iniciales',
+                data: [4,3,1,1,3],
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    const cakeGraph3 = {
+        labels: ['Totalmente De Acuerdo', 'De Acuerdo', 'Ni Acuerdo ni Desacuerdo', 'Desacuerdo', 'Totalmente en Desacuerdo'],
+        datasets: [
+            {
+                label: 'Resultados Cuestionario Inicial - Pensamiento',
+                data: [7,0,1,2,2],
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    const cakeGraph4 = {
+        labels: ['Totalmente De Acuerdo', 'De Acuerdo', 'Ni Acuerdo ni Desacuerdo', 'Desacuerdo', 'Totalmente en Desacuerdo'],
+        datasets: [
+            {
+                label: 'Resultados Cuestionario Inicial - Investigación',
+                data: [7,4,1,2,2],
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    
+
     useEffect(() => {
         
     }, []);
     
     return (
-        <>
-            hola
-        </>
-    )
+        <div>
+            <DashboardLayout>
+            <div className={styles.pageContent}>
+                <h1>¡Hola!</h1>
+                <div className={styles.chartContainer}>
+                    <div className={styles.chart}>
+                        <Doughnut data={cakeGraph} />
+                    </div>
+                    <div className={styles.chart}>
+                        <Doughnut data={cakeGraph2} />
+                    </div>
+                    <div className={styles.chart}>
+                        <Doughnut data={cakeGraph3} />
+                    </div>
+                    <div className={styles.chart}>
+                        <Doughnut data={cakeGraph4} />
+                    </div>
+                </div>
+            </div>
+            </DashboardLayout>
+        </div>
+    );
 }
