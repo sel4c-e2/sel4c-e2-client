2a53611356b11755329ea977e28d936e801c63f1
Esteban Zepeda <109043513+estzep@users.noreply.github.com>
2023-10-16 17:27:27 -0600

    Update

--- src/components/Layouts/DashboardLayout.jsx
+++ src/components/Layouts/DashboardLayout.jsx
@@ -4,7 +4,7 @@ export default function DashboardLayout({ children }) {
     return (
         <section className={style.section}>
             <div className={style.sectionContainer + ' container'}>
-                <div className={style.content}>
+                <div className={style.content + ' row'}>
                     {children}
                 </div>
             </div>
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-// const SERVER_URL = 'http://localhost:3001';
-const SERVER_URL = 'https://sel4c-e2-server-49c8146f2364.herokuapp.com';
+const SERVER_URL = 'http://localhost:3001';
+// const SERVER_URL = 'https://sel4c-e2-server-49c8146f2364.herokuapp.com';
 
 module.exports = {
     SERVER_URL
